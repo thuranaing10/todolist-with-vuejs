d09d8ee72dbc5fefa28411267489211db8d57c71
thuranaing10 <thura235.mm@gmail.com>
2022-03-05 00:41:18 +0630

    before vuex add

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,85 @@
 <template>
-    <div class="container mt-5">
-        <notifications group="foo" class="mt-3" />
-        <AddTask @addTask="addTask"/>
-        <TransitionGroup 
-            enter-active-class="animate__animated animate__bounceInLeft"
-            leave-active-class="animate__animated animate__bounceOutRight"
-            class="row justify-content-center mt-3"
-        >
-            <TodoItem  v-for="(todo,index) in todos" :key="index" :todo="todo" :index="index" @completedTodo="completedTodo" @deleteTodo="deleteTodo" @editTodo="editTodo" />
-        </TransitionGroup>
-    </div>
+  <div class="container mt-5">
+    <notifications group="foo" class="mt-3" />
+    <AddTask @addTask="addTask" />
+    <TransitionGroup
+      enter-active-class="animate__animated animate__bounceInLeft"
+      leave-active-class="animate__animated animate__bounceOutRight"
+      class="row justify-content-center mt-3"
+    >
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+        :index="index"
+        @completedTodo="completedTodo"
+        @deleteTodo="deleteTodo"
+        @editTodo="editTodo"
+      />
+    </TransitionGroup>
+  </div>
 </template>
 
 <script>
-import AddTask from './components/AddTask.vue';
-import TodoItem from './components/TodoItem.vue';
+import AddTask from "./components/AddTask.vue";
+import TodoItem from "./components/TodoItem.vue";
 export default {
-    name: 'App',
-    components: { 
-        AddTask,TodoItem
+  name: "App",
+  components: {
+    AddTask,
+    TodoItem,
+  },
+  data: () => ({
+    todos: [],
+  }),
+  methods: {
+    addTask(todo) {
+      this.todos.push(todo);
+      this.$notify({
+        group: "foo",
+        title: "Deleted",
+        text: "The Todo has been created successfully!",
+        type: "success",
+      });
     },
-    data:()=>({
-        todos:[],
-    }),
-    methods:{
-        addTask(todo){
-            this.todos.push(todo);
-            this.$notify({
-                    group: 'foo',
-                    title: 'Deleted',
-                    text: 'The Todo has been created successfully!',
-                    type: 'success',
-                });
-        },
-        deleteTodo(index){
-            if(confirm('Are you sure to delete?')){
-                this.todos.splice(index,1);
-                this.$notify({
-                    group: 'foo',
-                    title: 'Deleted',
-                    text: 'The Todo has been deleted successfully!',
-                    type: 'error',
-                });
-            }
-                
-        },
-        completedTodo(index){
-            this.todos[index].done = true;
-            this.$notify({
-                    group: 'foo',
-                    title: 'Completed',
-                    text: 'The Todo has been completed successfully!',
-                    type: 'success',
-                });
-        },
-        editTodo({todo,index}){
-            this.todos[index] = todo;
-            this.$notify({
-                    group: 'foo',
-                    title: 'Updated',
-                    text: 'The Todo has been updated successfully!',
-                    type: 'success',
-                });
-        }
-    }
-}
+    deleteTodo(index) {
+      if (confirm("Are you sure to delete?")) {
+        this.todos.splice(index, 1);
+        this.$notify({
+          group: "foo",
+          title: "Deleted",
+          text: "The Todo has been deleted successfully!",
+          type: "error",
+        });
+      }
+    },
+    completedTodo(index) {
+      this.todos[index].done = true;
+      this.$notify({
+        group: "foo",
+        title: "Completed",
+        text: "The Todo has been completed successfully!",
+        type: "success",
+      });
+    },
+    editTodo({ todo, index }) {
+      this.todos[index] = todo;
+      this.$notify({
+        group: "foo",
+        title: "Updated",
+        text: "The Todo has been updated successfully!",
+        type: "success",
+      });
+    },
+  },
+};
 </script>
 
 <style>
-    label{
-        color: white;
-    }
-    .card{
-        border-radius: 1rem !important;
-    }
-    
+label {
+  color: white;
+}
+.card {
+  border-radius: 1rem !important;
+}
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,50 +1,55 @@
 <template>
-    <div class="col-11 mt-3">
-        <div class="card">
-            <div class="row p-3 justify-items-center">
-                <div class="col-7 mt-3" :class="{'taskCompleted':todo.done}" @dblclick="editToggle = !editToggle">
-                    <input type="text" class="form-control" v-if="editToggle" v-model="todo.task" @keyup.enter="editTodo">
-                    <span v-else>{{todo.task}}</span>
-                </div>
-                <div class="col text-right">
-                    <button class="btn btn-success m-3" @click="completedTodo(index)">
-                        <span class="material-icons">
-                            check
-                            </span>
-                    </button>
-                    <button @click="deleteTodo(index)" class="btn btn-danger">
-                        <span class="material-icons">
-                            delete
-                            </span>
-                    </button>
-                </div>
-            </div>
+  <div class="col-11 mt-3">
+    <div class="card">
+      <div class="row p-3 justify-items-center">
+        <div
+          class="col-7 mt-3"
+          :class="{ taskCompleted: todo.done }"
+          @dblclick="editToggle = !editToggle"
+        >
+          <input
+            type="text"
+            class="form-control"
+            v-if="editToggle"
+            v-model="todo.task"
+            @keyup.enter="editTodo"
+          />
+          <span v-else>{{ todo.task }}</span>
         </div>
+        <div class="col text-right">
+          <button class="btn btn-success m-3" @click="completedTodo(index)">
+            <span class="material-icons"> check </span>
+          </button>
+          <button @click="deleteTodo(index)" class="btn btn-danger">
+            <span class="material-icons"> delete </span>
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default{
-    name:"TodoItem",
-    props:["todo","index"],
-    components:{
+export default {
+  name: "TodoItem",
+  props: ["todo", "index"],
+  components: {},
+  data() {
+    return {
+      editToggle: false,
+    };
+  },
+  methods: {
+    completedTodo(index) {
+      this.$emit("completedTodo", index);
     },
-    data(){
-        return{ 
-            editToggle:false
-        }
+    deleteTodo(index) {
+      this.$emit("deleteTodo", index);
     },
-    methods:{
-        completedTodo(index){
-            this.$emit('completedTodo',index);
-        },
-        deleteTodo(index){
-            this.$emit('deleteTodo',index);
-        },
-        editTodo(){
-            this.$emit('editTodo',{todo:this.todo,index:this.index});
-            this.editToggle = false;
-        }
-    }
-}
+    editTodo() {
+      this.$emit("editTodo", { todo: this.todo, index: this.index });
+      this.editToggle = false;
+    },
+  },
+};
 </script>
